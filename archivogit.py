1ce67476f62d9a48c2ecac0c751dc7233eb897d2
JDeveloperJ <johann1318@hotmail.com>
2022-07-31 14:06:53 -0500

    Version 1 aGit

---
+++ archivogit.py
@@ -0,0 +1,22 @@
+print("Hola como estas")
+print("1.sumar\n2.Restar\n3.multiplicar")
+opcion = int(input("Ingresa una opcion a realizar"))
+numero1 = int(input("Ingresa el primer numero"))
+numero2 = int(input("Ingresa el segundo numero"))
+
+if opcion == 1:
+    suma = numero1 + numero2
+    print("La suma es:" , suma)
+elif opcion == 2:
+    if(numero1 < numero2):
+        resta = numero2 - numero1
+        print("La resta es: " , resta)
+    else: 
+        resta = numero1 - numero2
+        print("La resta es: ", resta)
+else:
+    mutiplicacion = numero1 * numero2
+    print("La multiplicacion es es: " , mutiplicacion)
+
+
+
